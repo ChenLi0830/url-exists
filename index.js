0c2f3ff6fa919c2889a0c76c50e5ec63d7758208
Chen Li <yichen.li0830@gmail.com>
2017-11-10 21:42:40 -0800

    Add support for promise

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 var request = require('request');
+var requestPromise = require('request-promise');
 
 function urlExists(url, cb) {
-  request({ url: url, method: 'HEAD' }, function(err, res) {
-    if (err) return cb(null, false);
-    cb(null, /4\d\d/.test(res.statusCode) === false);
-  });
-}
+  if (cb) {
+    request({ url: url, method: 'HEAD' }, function(err, res) {
+      if (err) return cb(null, false);
+      cb(null, /4\d\d/.test(res.statusCode) === false);
+    });
+  }
+  else {
+    return requestPromise({ url: url, method: 'HEAD' })
+    .then(function (res) {
+      return /4\d\d/.test(res.statusCode) === false;
+    })
+    .catch(function (err) {
+      return false;
+    });
+  };
+};
 
 module.exports = urlExists;
--- test/index.js
+++ test/index.js
@@ -2,21 +2,43 @@ var assert = require('assert');
 
 var urlExists = require('..');
 
-urlExists('https://www.google.com', function(err, exists) {
-  assert(exists === true);
-
-  urlExists('https://google.com', function(err, exists) {
+urlExists('https://www.google.com')
+  .then(exists => {
     assert(exists === true);
-
-    urlExists('http://www.google.com', function(err, exists) {
+    return urlExists('https://google.com')
+  })
+  .then(exists => {
+    assert(exists === true);
+    return urlExists('http://www.google.com')
+  })
+  .then(exists => {
+    assert(exists === true);
+    return urlExists('http://www.google.com')
+  })
+  .then(exists => {
+    assert(exists === true);
+    return urlExists('https://www.asdflkasdfljalsfdjasfdljklsjafasdfljjkasfdsafdljfdsaljakljsdljksafasfdlk.lasjkd')
+  })
+  .then(exists => {
+    assert(exists === false);
+  })
+  .then(() => {
+    urlExists('https://www.google.com', function(err, exists) {
       assert(exists === true);
-
-      urlExists('https://www.asdflkasdfljalsfdjasfdljklsjafasdfljjkasfdsafdljfdsaljakljsdljksafasfdlk.lasjkd', function(err, exists) {
-        assert(exists === false);
-
-        console.log('All tests pass!');
-        process.exit(0);
+    
+      urlExists('https://google.com', function(err, exists) {
+        assert(exists === true);
+    
+        urlExists('http://www.google.com', function(err, exists) {
+          assert(exists === true);
+    
+          urlExists('https://www.asdflkasdfljalsfdjasfdljklsjafasdfljjkasfdsafdljfdsaljakljsdljksafasfdlk.lasjkd', function(err, exists) {
+            assert(exists === false);
+    
+            console.log('All tests pass!');
+            process.exit(0);
+          });
+        });
       });
     });
   });
-});
